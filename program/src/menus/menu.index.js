8ee9d0fb345b4826801598eedaa5e7fd79154762
Ludmila Gomes <ludmila.gomes490@gmail.com>
2023-11-07 08:31:25 -0300

    Delete program/src/menus directory

--- program/src/menus/menu.index.js
+++
@@ -1,7 +0,0 @@
-import {submenuAutor} from './menuAutor.js';
-import {submenuEditora} from './menuEditora.js';
-import {submenuLivro} from './menuLivro.js';
-import {submenuRelatorios} from './menuRelat.js';
-import {waitForEnter, showMenu, promptMenu} from './menuFuncoes.js';
-
-export {submenuAutor, submenuEditora, submenuLivro, submenuRelatorios, waitForEnter, showMenu, promptMenu};
